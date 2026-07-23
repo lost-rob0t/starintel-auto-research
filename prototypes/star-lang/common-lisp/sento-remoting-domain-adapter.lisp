6cdf5488f67e10d50b018230aa4da9851d5a0322
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-07-23 11:18:38 -0400

    Expose Sento bound runtime port

--- prototypes/star-lang/common-lisp/sento-remoting-domain-adapter.lisp
+++ prototypes/star-lang/common-lisp/sento-remoting-domain-adapter.lisp
@@ -3,26 +3,32 @@
 (export '(make-sento-remoting-domain-port))
 
 (defun make-sento-remoting-domain-port ()
-  (make-domain-remoting-port
-   :enable
-   (lambda (system options)
-     (apply #'rem:enable-remoting system options))
-   :actor-of
-   (lambda (system name receive options)
-     (apply #'ac:actor-of
-            system
-            :name name
-            :receive receive
-            options))
-   :remote-ref
-   (lambda (system uri options)
-     (apply #'rem:make-remote-ref system uri options))
-   :tell
-   (lambda (actor message sender)
-     (act:tell actor message sender))
-   :stop
-   (lambda (system actor)
-     (ac:stop system actor))
-   :disable
-   (lambda (system)
-     (rem:disable-remoting system))))
+  (let ((port
+          (make-domain-remoting-port
+           :enable
+           (lambda (system options)
+             (apply #'rem:enable-remoting system options))
+           :actor-of
+           (lambda (system name receive options)
+             (apply #'ac:actor-of
+                    system
+                    :name name
+                    :receive receive
+                    options))
+           :remote-ref
+           (lambda (system uri options)
+             (apply #'rem:make-remote-ref system uri options))
+           :tell
+           (lambda (actor message sender)
+             (act:tell actor message sender))
+           :stop
+           (lambda (system actor)
+             (ac:stop system actor))
+           :disable
+           (lambda (system)
+             (rem:disable-remoting system)))))
+    (register-domain-remoting-runtime-port
+     port
+     (lambda (system)
+       (rem:remoting-port system)))
+    port))
